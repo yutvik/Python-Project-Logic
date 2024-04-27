c2e973356a29a6fe8d26fda505aff3de7aaaf706
yutvik <yutvilsavani@gmail.com>
2024-04-27 10:01:53 +0530

    testing

--- veg/models.py
+++ veg/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
     recipe_description = models.TextField()
-    image = models.ImageField(upload_to="recipe")
+    # image = models.ImageField(upload_to="recipe")
+    
